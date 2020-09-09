fdb8c37d3b8886832d3fb70997e44851800efece
Alex Popov <yolloartstudio@gmail.com>
2020-09-09 11:40:29 +0300

    Initial Commit

---
+++ ContactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by YolloArts on 09.09.2020.
+//  Copyright © 2020 YolloArts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
